69cfc2f0db1cb92e437f318ed3aa5307d67ff200
Alexander Höreth <a.hoereth@gmail.com>
2015-09-09 14:46:28 +0200

    Access to logs.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p logs
 
 dir=tmp
 mkdir -p $dir
--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ docker run \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   -v $HOME/.Xauthority:/home/developer/.Xauthority:ro \
+  -v `pwd`/logs:/home/shopper/logs:rw \
   --net=host \
   shopping_scenario
